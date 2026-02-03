d3a909bb30bae48845dce037fa8093778ee96ba9
abhishekrajput2410 <abhishekkumarrajput2410@gmail.com>
2026-02-03 14:33:31 +0530

    03/02/2026

---
+++ lecture1.js
@@ -0,0 +1,6 @@
+import { add } from "./utils.js";
+import { subtract } from "./utils.js";
+const result = add(2, 3);
+const result1 = subtract(5, 2); 
+console.log("The sum is:", result);
+console.log("The difference is:", result1);

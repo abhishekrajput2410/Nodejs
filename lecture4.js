2b626c9061346cb96896a35acbf79e7a29ed11b9
abhishekrajput2410 <abhishekkumarrajput2410@gmail.com>
2026-02-13 11:43:05 +0530

    13/02/2026

--- lecture4.js
+++ lecture4.js
@@ -26,15 +26,15 @@ const getUserName = () => {
     return Promise.resolve("User name fetched!");
 };
 
-// login().then(() =>{
-//     getUserData().then(() => {
-//         getUserName().then((result) => {
-//             console.log("Final Result:", result);
-//         }).catch((error) => {
-//             console.error("Error in getUserName:", error);
-//         });
-//     });
-// });
+login().then(() =>{
+    getUserData().then(() => {
+        getUserName().then((result) => {
+            console.log("Final Result:", result);
+        }).catch((error) => {
+            console.error("Error in getUserName:", error);
+        });
+    });
+});
 
 
 login()
@@ -46,15 +46,28 @@ login()
 
 
 Promise.resolve(10)
-.then((x) => x + 1)
-.then((x) => x * 2)
+.then((x) => {
+    x + 1; // We get 11 here, but we are not returning it, so the next then will receive undefined.
+})
+.then((x) => x * 2) // Since the previous then did not return anything, x is undefined here, and this will result in NaN.
 .then((x) => {
     console.log("Final value:", x);
     return x + 8; // This value is not returned, so it will be undefined in the next then..
 }).catch((error) => console.error("Error in promise chain:", error));
 
-// const data = () => 10
-// const data1 = () => (10)
-// const newData = (value) => {
-//     if( value === 10) return 10;
-// };
+const data = () => 10
+const data1 = () => (10)
+const newData = (value) => {
+    if( value === 10) return 10;
+};
+
+const fetchdata = async () => { // It will return a promise and allows us to use await inside it.
+    console.log("Fetching data...");
+    await fetch("https://jsonplaceholder.typicode.com/posts/1") //  We use await to wait for this promise to resolve before moving on to the next line of code.
+    .then(response => response.json()) // We can also use await here instead of chaining then. 
+    .then(data => console.log("Fetched data:", data)) // We can also use await here instead of chaining then.
+    .catch(error => console.error("Error fetching data:", error)) // We can also use try-catch block for error handling instead of catch.
+    .finally(() => console.log("Data fetched successfully!"));  // The finally block will execute regardless of whether the promise was resolved or rejected.
+    
+};
+fetchdata();
